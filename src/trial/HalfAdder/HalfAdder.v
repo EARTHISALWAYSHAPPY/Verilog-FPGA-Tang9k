0c4873b7cd6adba10c5c38c53a79facedbacd1af
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-06-03 14:03:19 +0700

    fix

--- src/trial/HalfAdder/HalfAdder.v
+++ src/trial/HalfAdder/HalfAdder.v
@@ -1,7 +1,9 @@
-module HalfAdder(
-    input wire iAugend, iAddend,
-    output wire oSum, oCarry
+module HalfAdder (
+    input  wire iAugend,
+    input  wire iAddend,
+    output wire oSum,
+    output wire oCarry
 );
-    assign oSum = iAugend ^ iAddend;  // XOR
-    assign oCarry = iAugend & iAddend; // AND
+  assign oSum   = iAugend ^ iAddend;  // XOR
+  assign oCarry = iAugend & iAddend;  // AND
 endmodule
