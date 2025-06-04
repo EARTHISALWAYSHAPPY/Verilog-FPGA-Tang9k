7e6c035b6d6a63936bbb07f181e90d885a5ca03c
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-06-04 08:26:49 +0700

    fix II

--- src/trial/Blinker/Blinker.v
+++ src/trial/Blinker/Blinker.v
@@ -1,29 +1,27 @@
 module Blinker (
-    input wire CLK,
-    input wire RESETn,
+    input  wire CLK,
+    input  wire RESETn,
     output wire oLED
 );
-    reg rLED;
-    reg [2:0] rCnt;
+  reg rLED;
+  reg [2:0] rCnt;
 
-    assign oLED = rLED;
+  assign oLED = rLED;
 
-    always @(posedge CLK or negedge RESETn) begin : u_rCnt
+  always @(posedge CLK or negedge RESETn) begin : u_rCnt
     if (RESETn == 1'd0) begin
-        rCnt <= 3'd0;
+      rCnt <= 3'd0;
+    end else begin
+      rCnt <= rCnt + 3'd1;
     end
-    else begin
-        rCnt <= rCnt + 3'd1;
-    end
-    end
+  end
 
-    always @(posedge CLK or negedge RESETn) begin : u_rLED
+  always @(posedge CLK or negedge RESETn) begin : u_rLED
     if (RESETn == 1'd0) begin
-        rLED <= 1'd0;
+      rLED <= 1'd0;
+    end else begin
+      rLED <= (rCnt == 3'd7) ? ~rLED : rLED;
     end
-    else begin
-        rLED <= (rCnt == 3'd7) ? ~rLED : rLED;
-    end
-    end
+  end
 
 endmodule
