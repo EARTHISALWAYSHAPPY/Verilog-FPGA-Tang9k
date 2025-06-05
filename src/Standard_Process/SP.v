48477da28d960add086fa7cb7877626ec24874b1
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-06-05 12:08:18 +0700

    update

---
+++ src/Standard_Process/SP.v
@@ -0,0 +1,38 @@
+//File name : 
+//Description : 
+//Company :
+//Project :
+//-------------------     |     NEW
+//Version :               |
+//Date :                  |
+//Author :                |
+//Remark :                |   
+//-------------------     |   
+//Version :               |
+//Date :                  |
+//Author :                |
+//Remark :                |     OLD
+                          V
+module moduleName (
+   
+);
+//-------------------  
+// Constant Declaration 
+// for initial Const.
+//-------------------
+
+//-------------------  
+// Signal Declaration 
+// for initial reg,wire
+//-------------------
+
+//-------------------  
+// Output 
+// assignd wire <= reg
+//-------------------
+
+//-------------------  
+// Process Declaration
+// @always
+//-------------------
+endmodule
