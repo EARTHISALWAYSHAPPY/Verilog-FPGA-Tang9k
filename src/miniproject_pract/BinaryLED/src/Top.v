33702a6bd94931eea0c6df95908f03a7fbb435e1
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-06-20 20:36:55 +0700

    fix comment

--- src/miniproject_pract/BinaryLED/src/Top.v
+++ src/miniproject_pract/BinaryLED/src/Top.v
@@ -4,6 +4,7 @@ module BinaryLED (
     output wire [5:0] oled
 );
   localparam Timedelay = 25'd26_999_999;
+  // what time I need = Clock cyc. / Sys. Freq. (27MHz.)
   reg [24:0] rCnt;
   reg [ 5:0] rled;
 
