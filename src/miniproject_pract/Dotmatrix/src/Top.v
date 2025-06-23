1cc7a75827d3bdd753db21f9a1c22963bd33a390
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-06-23 14:10:27 +0700

    add dotmatrix

--- src/miniproject_pract/Dotmatrix/src/Top.v
+++ src/miniproject_pract/Dotmatrix/src/Top.v
@@ -5,35 +5,48 @@ module Top (
     output wire [7:0] Col_LED
 );
 
-  localparam Timedelay = 25'd26999999;  // 1 second
+  localparam Timedelay = 25'd27000;
   reg [24:0] rCnt;
-  reg [ 7:0] rRow_LED;
-  reg [ 7:0] rCol_LED;
+  reg [7:0] rRow_LED;
+  reg [7:0] rCol_LED;
+  reg [2:0] Cnt_row;
 
+  reg [7:0] char_a[0:7];
+  initial begin
+    char_a[0] = 8'b11101100;
+    char_a[1] = 8'b10010100;
+    char_a[2] = 8'b10000110;
+    char_a[3] = 8'b01000001;
+    char_a[4] = 8'b01000001;
+    char_a[5] = 8'b10000110;
+    char_a[6] = 8'b10010100;
+    char_a[7] = 8'b11101100;
+  end
 
-  always @(posedge CLK or negedge RESETn) begin : u_rCnt
-    if (RESETn == 1'd0) begin
+  always @(posedge CLK or negedge RESETn) begin
+    if (!RESETn) begin
+      rCnt <= 25'd0;
+    end else if (rCnt == Timedelay) begin
       rCnt <= 25'd0;
     end else begin
-      if (rCnt < Timedelay) begin
-        rCnt <= rCnt + 1;
-      end else begin
-        rCnt <= 25'd0;
-      end
+      rCnt <= rCnt + 25'd1;
     end
   end
 
-  always @(posedge CLK or negedge RESETn) begin : u_rDotmatix
-    if (RESETn == 1'd0) begin
-      rCol_LED <= 8'b00000000;
-      rRow_LED <= 8'b00000000;
-    end else begin
-      rCol_LED <= 8'b11111111;
-      rRow_LED <= 8'b00000000;
+  always @(posedge CLK or negedge RESETn) begin
+    if (!RESETn) begin
+      Cnt_row <= 3'd0;
+    end else if (rCnt == Timedelay) begin
+      Cnt_row <= (Cnt_row == 3'd7) ? 3'd0 : Cnt_row + 3'd1;
     end
   end
 
+  always @(*) begin
+    rCol_LED = (8'b00000001 << Cnt_row);
+    rRow_LED = ~char_a[Cnt_row];
+  end
 
   assign Row_LED = rRow_LED;
   assign Col_LED = rCol_LED;
 endmodule
+
