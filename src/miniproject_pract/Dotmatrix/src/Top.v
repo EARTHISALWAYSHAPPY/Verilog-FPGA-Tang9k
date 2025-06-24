9f1727f47b3dd9f0e992de689e6ebf3f347e9151
EARTHISALWAYSHAPPY <eathdis@gmail.com>
2025-06-24 19:07:59 +0700

    fix bug

--- src/miniproject_pract/Dotmatrix/src/Top.v
+++ src/miniproject_pract/Dotmatrix/src/Top.v
@@ -5,48 +5,49 @@ module Top (
     output wire [7:0] Col_LED
 );
 
-  localparam Timedelay = 25'd27000;
-  reg [24:0] rCnt;
+  //wanna be 120 Hz
+  localparam Scanrate = 25'd22500;
+  reg [18:0] rCnt;
+
   reg [7:0] rRow_LED;
   reg [7:0] rCol_LED;
+
   reg [2:0] Cnt_row;
 
   reg [7:0] char_a[0:7];
   initial begin
-    char_a[0] = 8'b11101100;
-    char_a[1] = 8'b10010100;
-    char_a[2] = 8'b10000110;
-    char_a[3] = 8'b01000001;
-    char_a[4] = 8'b01000001;
-    char_a[5] = 8'b10000110;
-    char_a[6] = 8'b10010100;
-    char_a[7] = 8'b11101100;
+    char_a[0] = 8'b00000000;
+    char_a[1] = 8'b01100110;
+    char_a[2] = 8'b11111111;
+    char_a[3] = 8'b11111111;
+    char_a[4] = 8'b11111111;
+    char_a[5] = 8'b01111110;
+    char_a[6] = 8'b00111100;
+    char_a[7] = 8'b00011000;
   end
 
-  always @(posedge CLK or negedge RESETn) begin
+  always @(posedge CLK or negedge RESETn) begin : u_rCnt
     if (!RESETn) begin
-      rCnt <= 25'd0;
-    end else if (rCnt == Timedelay) begin
-      rCnt <= 25'd0;
+      rCnt <= 18'd0;
+    end else if (rCnt == Scanrate) begin
+      rCnt <= 18'd0;
     end else begin
-      rCnt <= rCnt + 25'd1;
+      rCnt <= rCnt + 18'd1;
     end
   end
 
-  always @(posedge CLK or negedge RESETn) begin
+  always @(posedge CLK or negedge RESETn) begin : u_rDotmatrix
     if (!RESETn) begin
       Cnt_row <= 3'd0;
-    end else if (rCnt == Timedelay) begin
-      Cnt_row <= (Cnt_row == 3'd7) ? 3'd0 : Cnt_row + 3'd1;
+    end else if (rCnt == Scanrate) begin
+      Cnt_row  <= (Cnt_row == 3'd7) ? 3'd0 : Cnt_row + 3'd1;
+      //rCol_LED = 8'b11111111;
+      rCol_LED <= (8'b00000001 << Cnt_row);
+      rRow_LED <= ~char_a[Cnt_row];
     end
-  end
-
-  always @(*) begin
-    rCol_LED = (8'b00000001 << Cnt_row);
-    rRow_LED = ~char_a[Cnt_row];
   end
 
   assign Row_LED = rRow_LED;
   assign Col_LED = rCol_LED;
-endmodule
 
+endmodule
